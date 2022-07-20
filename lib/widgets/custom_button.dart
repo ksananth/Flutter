a5461adac91653967e41d345f640f98cb7be766b
Ananth <anan.busyguy007@gmail.com>
2022-07-21 00:12:15 +0200

    Add border to button

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,32 +1,27 @@
-
-
 import 'package:flutter/material.dart';
 
-class CustomButton extends StatelessWidget{
-
+class CustomButton extends StatelessWidget {
   const CustomButton({required this.title});
 
   final String title;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox.fromSize(
-      size: const Size(56, 56), // button width and height
-      child: ClipRect(
-        child: Material(
-          color: Colors.orange, // button color
-          child: InkWell(
-            splashColor: Colors.green, // splash color
-            onTap: () {}, // button pressed
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children:  [
-                Text(title, style: const TextStyle(fontSize: 16, color: Colors.blue)), // text
-              ],
-            ),
-          ),
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        onPrimary: Colors.black,
+        animationDuration: const Duration(milliseconds: 700),
+        primary: Colors.transparent,
+        shadowColor: Colors.white54,
+        side: const BorderSide(color: Colors.blueAccent),
+        elevation: 10,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5),
         ),
       ),
+      child:
+      Text(title, style: const TextStyle(fontSize: 12, color: Colors.black26)), // text
     );
   }
 }
