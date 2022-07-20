8b4895960f4807212bac84d7049afd4805f74c16
Ananth <anan.busyguy007@gmail.com>
2022-07-20 22:47:05 +0200

    Add tabs

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/utils/content_view.dart';
+import 'package:my_app/utils/view_wrapper.dart';
 import 'package:my_app/views/home_view.dart';
+import 'package:my_app/widgets/Custom_tab_bar.dart';
 import 'package:my_app/widgets/custom_tab.dart';
 import 'package:my_app/views/about_view.dart';
 import 'package:my_app/views/project_view.dart';
+
+import 'utils/tab_controller_handler.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -14,9 +18,16 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
   late TabController tabController;
+  var scaffoldKey = GlobalKey<ScaffoldState>();
+
+  late double screenHeight;
+  late double screenWidth;
+  late double topPadding;
+  late double bottomPadding;
+  late double sidePadding;
 
   List<ContentView> contentViews = [
     ContentView(
@@ -34,30 +45,75 @@ class _MyHomePageState extends State<MyHomePage> {
   ];
 
   @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: contentViews.length, vsync: this);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    screenWidth = MediaQuery.of(context).size.width;
+    screenHeight = MediaQuery.of(context).size.height;
+    topPadding = screenHeight * 0.05;
+    bottomPadding = screenHeight * 0.03;
+    sidePadding = screenWidth * 0.05;
+
+    print('Width: $screenWidth');
+    print('Height: $screenHeight');
+
     return Scaffold(
+      key: scaffoldKey,
+      //endDrawer: drawer(),
       appBar: AppBar(
           elevation: 15,
           shadowColor: Colors.orangeAccent,
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
           title: const SizedBox(
             width: 40,
             child: Icon(Icons.sailing),
           ),
           centerTitle: false),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Home Screen',
-            ),
-          ],
-        ),
+      body: Padding(
+        padding: EdgeInsets.only(top: topPadding, bottom: bottomPadding),
+        child:
+            ViewWrapper(desktopView: desktopView(), mobileView: mobileView()),
       ),
+    );
+  }
+
+  Widget mobileView() {
+    return Column(
+      children: [
+        Container(
+          color: Colors.blueGrey,
+          child: const Text("mobileView"),
+        )
+      ],
+    );
+  }
+
+  Widget desktopView() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children:  [
+        SizedBox(
+          height: screenHeight * 0.05,
+          child: CustomTabBar(
+              controller: tabController,
+              tabs: contentViews.map((e) => e.tab).toList()),
+        ),
+        SizedBox(
+          height: screenHeight * 0.8,
+          child: TabControllerHandler(
+            tabController: tabController,
+            key: null,
+            child: TabBarView(
+              controller: tabController,
+              children: contentViews.map((e) => e.content).toList(),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
