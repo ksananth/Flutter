0774eed712e65caf3d3d444f1804ed3a7dc295de
Ananth <anan.busyguy007@gmail.com>
2022-07-21 01:31:38 +0200

    Add man lottie

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:my_app/utils/view_wrapper.dart';
 
 class HomeView extends StatefulWidget {
-
   @override
   _HomeViewState createState() => _HomeViewState();
 }
@@ -24,19 +24,11 @@ class _HomeViewState extends State<HomeView>
 
   Widget desktopView() {
     return Container(
-      color: Colors.grey[300],
+      color: Colors.white,
       child: Center(
         child: Container(
           padding: const EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            color: Colors.red[400],
-            borderRadius: const BorderRadius.all(
-              Radius.circular(8),
-            ),
-          ),
-          child: const Text(
-            'Home Desktop View',
-          ),
+          child: Lottie.asset('assets/man.json'),
         ),
       ),
     );
