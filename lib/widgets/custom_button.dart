56fef9279994f1de2e80ae9c00fcc3dcf0cc8198
Ananth <anan.busyguy007@gmail.com>
2022-07-21 01:24:01 +0200

    Add padding to button

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -8,22 +8,25 @@ class CustomButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {},
-      style: ElevatedButton.styleFrom(
-        alignment: alignment,
-        onPrimary: Colors.black,
-        animationDuration: const Duration(milliseconds: 700),
-        primary: Colors.transparent,
-        shadowColor: Colors.white54,
-        side: const BorderSide(color: Colors.blueAccent),
-        elevation: 10,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(5),
-        ),
-      ),
-      child:
-      Text(title, style: const TextStyle(fontSize: 12, color: Colors.black26)), // text
-    );
+    return Container(
+        margin: const EdgeInsets.only(right: 25),
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            alignment: alignment,
+            onPrimary: Colors.black,
+            animationDuration: const Duration(milliseconds: 700),
+            primary: Colors.transparent,
+            shadowColor: Colors.white54,
+            side: const BorderSide(color: Colors.blueAccent),
+            elevation: 10,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+          ),
+          child: Text(title,
+              style:
+                  const TextStyle(fontSize: 12, color: Colors.black26)), // text
+        ));
   }
 }
